369af5a181acef9cdba4845a3d754967f3bb9400
akanimax <akanimax@gmail.com>
2017-08-28 10:53:17 +0530

    Model added

--- Scripts/computation_graph/Graph1.py
+++ Scripts/computation_graph/Graph1.py
@@ -53,6 +53,6 @@ with graph.as_default():
 
     # define the loss for this model:
     # calculate the loss and optimize the network
-    loss = tf.div(tf.sqrt(tf.reduce_mean(tf.square(y_pred - y_true))), 2, name="loss") # claculate the mean square error loss
+    loss = tf.sqrt(tf.reduce_mean(tf.square(y_pred - y_true)), name="loss") # claculate the mean square error loss
 
     train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss, name="train_op") # using Adam optimizer for optimization
